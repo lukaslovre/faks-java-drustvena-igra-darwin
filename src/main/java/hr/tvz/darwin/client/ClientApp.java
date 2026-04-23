b20d538a9f10d305112b351f3c4a0276c6209417
Lukas <lovretic2002@gmail.com>
2026-04-23 14:10:52 +0200

    Initial commit: Code structure and entry files, javafx deps

---
+++ src/main/java/hr/tvz/darwin/client/ClientApp.java
@@ -0,0 +1,7 @@
+package hr.tvz.darwin.client;
+
+public class ClientApp {
+    public static void main(String[] args) {
+        System.out.println("Client UI is launching...");
+    }
+}
---
+++ src/main/java/hr/tvz/darwin/server/ServerApp.java
@@ -0,0 +1,7 @@
+package hr.tvz.darwin.server;
+
+public class ServerApp {
+    public static void main(String[] args) {
+        System.out.println("Server is starting on port 8080...");
+    }
+}
